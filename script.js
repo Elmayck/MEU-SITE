f48f889061ef0dd22cd67f36b199ea3a18b2fbff
Elmayck <andremayco20@gmail.com>
2025-07-07 23:53:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "Você clicou no botão! Parabéns, o site está funcionando!";
+}
+// Função que mostra a mensagem quando o botão for clicado
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "Você clicou no botão! Parabéns, o site está funcionando!";
+}
